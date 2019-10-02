a2750f2399cb43bbaae98f53a3771701224865d8
Rennie Bevineau <rsbevineau@gmail.com>
2019-10-01 20:17:58 -0400

    Updates

---
+++ precache-manifest.22af49a5e17b4e593f6818fb467546a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e72e54a25916af22c37f4f7074cffe",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "8a603bfda8749c896747",
+    "url": "/flashcards/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "23bcd4ddc822c9761584",
+    "url": "/flashcards/static/css/main.2c207e01.chunk.css"
+  },
+  {
+    "revision": "8a603bfda8749c896747",
+    "url": "/flashcards/static/js/2.2f42d09d.chunk.js"
+  },
+  {
+    "revision": "23bcd4ddc822c9761584",
+    "url": "/flashcards/static/js/main.4402e9cb.chunk.js"
+  },
+  {
+    "revision": "d643fff13256465921ce",
+    "url": "/flashcards/static/js/runtime-main.fbd7c946.js"
+  }
+]);
